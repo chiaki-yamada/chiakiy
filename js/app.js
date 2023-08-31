16f77676f6f736e8a8fbd7e8b62da335f338470a
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-08-31 16:30:11 -0700

    updated scroll fade

--- js/app.js
+++ js/app.js
@@ -7,4 +7,17 @@ ham.addEventListener('click', function() {
   nav.classList.toggle('active');
 });
 
+const reveals = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', function() {
+  const windowHeight = window.innerHeight;
+  for (let i = 0; i < reveals.length; i++) {
+    const revealTop = reveals[i].getBoundingClientRect().top;
+    const revealPoint = 150;
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('active');
+    } 
+  }
+});
+
 
