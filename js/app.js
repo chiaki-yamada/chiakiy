73f04a4739e96a277c8d6b961fcfae954f9dc746
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-09-12 16:43:18 -0700

    updated

--- js/app.js
+++ js/app.js
@@ -22,4 +22,23 @@ window.addEventListener('scroll', function() {
   }
 });
 
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 1,
+  spaceBetween: 15,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+
+});
 
